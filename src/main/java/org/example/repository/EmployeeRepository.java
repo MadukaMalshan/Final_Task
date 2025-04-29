1cceb037dd567ed19bcc0d70ac8d0c69e042ba94
MadukaMalshan <malshanmaduka8@gmail.com>
2025-04-29 10:40:11 +0530

    Complete impl and repository

--- src/main/java/org/example/repository/EmployeeRepository.java
+++ src/main/java/org/example/repository/EmployeeRepository.java
@@ -3,7 +3,15 @@ package org.example.repository;
 import org.example.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+    List<EmployeeEntity> findByName(String name);
+
+    List<EmployeeEntity> findByDepartment(String department);
+
+    List<EmployeeEntity> findByEmail(String email);
+
     // Custom query methods can be defined here if needed
     // For example, find employees by name or department
     // List<EmployeeEntity> findByName(String name);
--- src/main/java/org/example/service/impl/EmployeeImpl.java
+++ src/main/java/org/example/service/impl/EmployeeImpl.java
@@ -1,51 +1,79 @@
 package org.example.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.example.dto.Employee;
+import org.example.entity.EmployeeEntity;
+import org.example.repository.EmployeeRepository;
 import org.example.service.EmployeeService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeImpl implements EmployeeService {
+    private final ModelMapper modelMapper;
+    private final EmployeeRepository employeeRepository;
     @Override
     public List<Employee> getAllEmployees() {
-        return List.of();
+        List<Employee> employees = new ArrayList<>();
+        List<EmployeeEntity>employeeEntities =employeeRepository.findAll();
+        employeeEntities.forEach(employeeEntity -> {
+            employees.add(modelMapper.map(employeeEntity, Employee.class));
+        });
+        return employees;
     }
 
     @Override
     public Employee getEmployeeById(Long id) {
-        return null;
+        return modelMapper.map(employeeRepository.findById(id), Employee.class);
     }
 
     @Override
     public void createEmployee(Employee employee) {
-
+        employeeRepository.save(modelMapper.map(employee, EmployeeEntity.class));
     }
 
     @Override
     public void updateEmployee(Employee employee) {
-
+        employeeRepository.save(modelMapper.map(employee, EmployeeEntity.class));
     }
 
     @Override
     public void deleteEmployee(Long id) {
-
+        employeeRepository.deleteById(id);
     }
 
     @Override
     public List<Employee> getEmployeesByName(String name) {
-        return List.of();
+        List<Employee> employees = new ArrayList<>();
+        List<EmployeeEntity> employeeEntities = employeeRepository.findByName(name);
+        employeeEntities.forEach(employeeEntity -> {
+            employees.add(modelMapper.map(employeeEntity, Employee.class));
+        });
+        return employees;
     }
 
     @Override
     public List<Employee> getEmployeesByDepartment(String department) {
-        return List.of();
+        List<Employee> employees = new ArrayList<>();
+        List<EmployeeEntity> employeeEntities = employeeRepository.findByDepartment(department);
+        employeeEntities.forEach(employeeEntity -> {
+            employees.add(modelMapper.map(employeeEntity, Employee.class));
+        });
+        return employees;
     }
 
     @Override
     public List<Employee> getEmployeesByEmail(String email) {
-        return List.of();
+        List<Employee> employees = new ArrayList<>();
+        List<EmployeeEntity> employeeEntities = employeeRepository.findByEmail(email);
+        employeeEntities.forEach(employeeEntity -> {
+            employees.add(modelMapper.map(employeeEntity, Employee.class));
+        });
+        return employees;
     }
     // This class is empty and serves as a placeholder for future implementation
     // or as a base class for other service implementations.
